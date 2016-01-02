8b6d6961c4ddd25e5f674282949679b856d8def3
pachuco <Ifail@life.net>
2016-01-02 10:15:29 +0200

    'restrict' macro to UPPERCASE

--- src/syntrax/resampler.c
+++ src/syntrax/resampler.c
@@ -238,7 +238,7 @@ void resampler_write_pair(void *_r, sample_t ls, sample_t rs)
 }
 
 #ifdef _MSC_VER
-#define restrict __restrict
+#define RESTRICT __restrict
 #endif
 
 static const sample_t * resampler_inner_loop( resampler *r, sample_t** out_,
@@ -247,7 +247,7 @@ static const sample_t * resampler_inner_loop( resampler *r, sample_t** out_,
 	in_size -= write_offset;
 	if ( in_size > 0 )
 	{
-		sample_t* restrict out = *out_;
+		sample_t* RESTRICT out = *out_;
 		sample_t const* const in_end = in + in_size;
 		imp_t const* imp = r->imp;
 
@@ -293,7 +293,7 @@ static const sample_t * resampler_inner_loop( resampler *r, sample_t** out_,
 	return in;
 }
 
-#undef restrict
+#undef RESTRICT
 
 static int resampler_wrapper( resampler *r, sample_t out [], int* out_size,
 		sample_t const in [], int in_size )
