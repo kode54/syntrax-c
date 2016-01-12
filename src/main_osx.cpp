7a9f3242117bcdd3c5a83f4816392a4925f6bfd4
Christopher Snowhill <kode54@gmail.com>
2016-01-12 00:31:24 -0800

    Fix printing of info strings before playback.

--- src/main_osx.cpp
+++ src/main_osx.cpp
@@ -91,9 +91,9 @@ int main(int argc, const char* const* argv)
     initSubsong(player, 0);
 
     playerGetInfo(player, &info);
-    printf("Syntrax test player v0.0001 || %i/%i\n", info.selectedSubs+1, info.totalSubs);
-    printf("Title: %s\n", info.subsongName);
-    printf("\ro: %3u - r: %2u - c: %2u (%2u)", info.coarse, info.fine, info.channelsPlaying, info.channelsPlaying > max_channels ? max_channels = info.channelsPlaying : max_channels);
+    fprintf(stderr, "Syntrax test player v0.0001 || %i/%i\n", info.selectedSubs+1, info.totalSubs);
+    fprintf(stderr, "Title: %s\n", info.subsongName);
+    fprintf(stderr, "\ro: %3u - r: %2u - c: %2u (%2u)", info.coarse, info.fine, info.channelsPlaying, info.channelsPlaying > max_channels ? max_channels = info.channelsPlaying : max_channels);
 
     signal(SIGINT, signal_handler);
 
